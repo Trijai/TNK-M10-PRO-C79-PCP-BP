83dd907d87d248d5c0e275858904e88f09523b1d
Trijai <164654228+Trijai@users.noreply.github.com>
2024-04-17 17:47:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -102,13 +102,14 @@ def eval_fitness(generation, config):
             pipe.move()
 
             # Pass bird's y location (bird.y) and gap's location (gap between pipes i.e. pipe.gap)  to neaural network and get the output 
-             
+            output = net.activate((bird.y, pipe.gap))   
            
             # if output[0] is greter then 0.5 the move the bird up using bird.moveup() method
-             
+            if output[0] > 0.5: 
+                bird.moveup()  
 
             # increase genome fitness by 0.1 
-            
+            genome.fitness = genome.fitness + 0.1 
             
             screen.blit(images["base"],[groundx,550])
             score_text=score_font.render("Gen:"+str(gen)+" Genome:"+str(genomeCount), True, (0,0,255)) 
